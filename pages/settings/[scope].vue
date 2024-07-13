3214f4b56a3fdd1aa75cc4038fbc298e0f702a86
Joel <70848930+raizestudio@users.noreply.github.com>
2024-07-13 20:48:54 +0200

    feat: create settings page

--- pages/settings/[scope].vue
+++ pages/settings/[scope].vue
@@ -8,26 +8,23 @@
             Gestion des paramètres de l'application
           </p>
         </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        
+        <div>
+          <div class="flex flex-col gap-2">
+            <div v-for="settingValue, settingKey in coreStore.settings" class="flex">
+                <div class="join w-full">
+                  <label class="btn btn-sm join-item rounded-r-full w-1/4">{{settingKey}}</label>
+                  <input
+                    type="text"
+                    :placeholder="`${settingValue}`"
+                    class="input input-sm join-item"
+                  />
+                  <button class="btn btn-sm btn-warning join-item">Reset</button>
+                  <button class="btn btn-sm btn-success join-item">Sauvegarder</button>
+                </div>
+            </div>
+            
+          </div>
+        </div>
       </CardSection>
     </SectionFullScreen>
   </NuxtLayout>
@@ -41,6 +38,7 @@ import _ from "lodash";
 const router = useRouter();
 const route = useRoute();
 
+const coreStore = useCoreStore();
 import SectionFullScreen from '~/components/section/SectionFullScreen.vue';
 import CardSection from '~/components/card/CardSection.vue';
 
